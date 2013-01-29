e73dd6e545bb0d613101b433176b8a0bc1d13bc4
Jon Wahlström <ejfwahlstrom@gmail.com>
2013-01-29 15:18:18 +0100

    �ndrat lite

---
+++ GUI - Omgjord/SuperPower.cpp
@@ -0,0 +1,161 @@
+#include "SuperPower.h"
+
+
+SuperPower::SuperPower()
+{
+}
+
+
+SuperPower::~SuperPower()
+{
+}
+
+
+/*Funktioner som returnerar medlemsvariablernas värden*/
+int SuperPower::getCurrency()
+{
+	return mCurrency;
+}
+
+int SuperPower::getFood()
+{
+	return mFood;
+}
+
+int SuperPower::getGoods()
+{
+	return mGoods;
+}
+
+int SuperPower::getPopulation()
+{
+	return mPopulation;
+}
+
+int SuperPower::getTaxes()
+{
+	return mTaxes;
+}
+
+int SuperPower::getTech()
+{
+	return mTech;
+}
+
+int SuperPower::getNuclearWeapon()
+{
+	return mNuclearWeapon;
+}
+
+int SuperPower::getSpaceProgram()
+{
+	return mSpaceProgram;
+}
+
+int SuperPower::getSpyNetwork()
+{
+	return mSpyNetwork;
+}
+
+int SuperPower::getRound()
+{
+	return mRound;
+}
+//--------------------------------------------
+/*Funktioner som ger medlemsvariabler nya värden*/
+void SuperPower::setFood(int food)
+{
+	mFood = food;
+}
+
+void SuperPower::setGoods(int goods)
+{
+	mGoods = goods;
+}
+
+void SuperPower::setTaxes(int taxes)
+{
+	mTaxes = taxes;
+}
+
+void SuperPower::setTech(int tech)
+{
+	mTech = tech;
+}
+
+void SuperPower::setRound(int round)
+{
+	mRound = round;
+}
+//-----------------------------------------------------------
+
+/*Uppgraderar mCurrency med mTaxes multiplicerat med mPopulation*/
+void SuperPower::getTaxIncome()
+{
+	mCurrency += mTaxes * mPopulation;
+}
+
+/*Kontrollerar ifall det finns nog med mat till hela befolkningen*/
+bool SuperPower::enoughFood()
+{
+	if(mFood >= mPopulation)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void SuperPower::updateFood()
+{
+	/*	Om mängden mat är tillräcklig blir mängden mat subtraherad med antalet befolkningen i miljoner
+		Populationen ökar och om landet har mer eller lika mycket pengar som befolkning blir mIncreasePopulation true.
+		Den används sedan för att ge möjligheten att betala en viss summa för att öka befolkningen*/
+	if(enoughFood())
+	{
+		mFood -= mPopulation;
+		++mPopulation;
+		if(mCurrency >= mPopulation)
+		{
+			mIncreasePopulation = true;
+		}
+	}
+	/*	Om mängden mat är mindre än häflten av befolkningen tilldelas mFood noll
+		mPatriotism subtraheras även med två*/
+	else if(mFood <= mPopulation / 2)
+	{
+		mFood = 0;
+		mPatriotism -= 2;
+	}
+	/*	Om det inte finns tillräckligt med mat och mFood inte är lika med noll
+		ökar inte patriotismen men mFood tilldelas noll*/
+	else if(!enoughFood() && mFood != 0)
+	{
+		mFood = 0;
+	}
+	/*	Om inga av det överstående stämmer, innebär det att maten är lika med noll och befolkningen inte fått någon mat
+		Detta ger minus fyra i patriotism*/
+	else
+	{
+		mFood = 0;
+		mPatriotism -= 4;
+	}
+}
+/*Kontrollerar ifall det är möjligt att öka sin population*/
+bool SuperPower::enableToIncreasePopulation()
+{
+	return mIncreasePopulation;
+}
+/*	mCurrency subtraheras med mPopulation, befolkningen i miljoner.
+	Ökar befolkningen med ett, en miljon.
+	Och tilldelar boolean:en mIncreasePopulation till false.*/
+void SuperPower::increasePopulation()
+{
+	mCurrency -= mPopulation;
+
+	++mPopulation;
+
+	mIncreasePopulation = false;
+}
---
+++ GUI - Omgjord/SuperPower.h
@@ -0,0 +1,72 @@
+#ifndef SUPERPOWER_H
+#define SUPERPOWER_H
+
+#include <memory>
+
+class GUIText;
+
+enum PoliticalType
+{
+	CAPITALIST, COMMUNIST
+}; 
+
+class SuperPower
+{
+public:
+	SuperPower();
+	~SuperPower();
+
+	int			getFood();
+	int			getGoods();
+	int			getTech();
+	int			getTaxes();
+	int			getPopulation();
+	int			getNuclearWeapon();
+	int			getSpaceProgram();
+	int			getSpyNetwork();
+	int			getCurrency();
+	int			getRound();
+
+	PoliticalType getType();
+
+	void		setFood(int food);
+	void		setGoods(int goods);
+	void		setTech(int tech);
+	void		setTaxes(int taxes);
+	void		setRound(int round);
+
+	void		getTaxIncome();
+	void		updateFood();
+	bool		enoughFood();
+
+	bool		enableToIncreasePopulation();
+	void		increasePopulation();
+	
+	virtual void		upgradeNuclearWeapon() = 0;
+	virtual void		upgradeSpaceProgram() = 0;
+	virtual void		upgradeSpyNetwork() = 0;
+	virtual void		showGUI() = 0;
+	virtual void		hideGUI() = 0;
+protected:
+	PoliticalType mType;
+
+	int mRound;
+
+	int	mPatriotism;
+	int	mCurrency;
+	int	mPopulation;
+
+	int	mFood;
+	std::shared_ptr<GUIText> mFoodText;
+
+	int	mGoods;
+	int	mTech;
+	int	mTaxes;
+	int	mSpyNetwork;
+	int	mNuclearWeapon;
+	std::shared_ptr<GUIText> mNuclearText;
+	int	mSpaceProgram;
+	bool mIncreasePopulation;
+};
+
+#endif
